61b482d47ecbe525772c19b69c6e38b41a9901df
antoinelaul <antoinelaul@outlook.fr>
2015-12-08 20:44:58 +0100

    add simplified archi yoeman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name alaulanApp
+ * @description
+ * # alaulanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('alaulanApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
